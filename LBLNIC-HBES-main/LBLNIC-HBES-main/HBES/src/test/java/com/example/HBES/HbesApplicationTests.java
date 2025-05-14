cf80aa0a269b23b7879f7737b2c2c45f963cbd8f
isak180 <is.chacana@duocuc.cl>
2025-05-14 16:06:09 -0400

    Add files via upload

---
+++ LBLNIC-HBES-main/LBLNIC-HBES-main/HBES/src/test/java/com/example/HBES/HbesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HBES;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
