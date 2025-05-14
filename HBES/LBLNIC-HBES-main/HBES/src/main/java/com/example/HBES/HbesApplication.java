ef703b18f588d4452406cb71f09d01efa8665387
isak180 <is.chacana@duocuc.cl>
2025-05-14 16:08:54 -0400

    Add files via upload

---
+++ HBES/LBLNIC-HBES-main/HBES/src/main/java/com/example/HBES/HbesApplication.java
@@ -0,0 +1,13 @@
+package com.example.HBES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbesApplication.class, args);
+	}
+
+}
