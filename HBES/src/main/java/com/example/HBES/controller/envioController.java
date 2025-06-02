47fc0f925281142c0e31ed8508a7261fd5c31c54
Momonga <lu.belmar@duocuc.cl>
2025-06-02 15:51:04 -0400

    Wipe.1

--- HBES/src/main/java/com/example/HBES/controller/envioController.java
+++
@@ -1,5 +0,0 @@
-package com.example.HBES.controller;
-
-public class envioController {
-
-}
--- HBES/src/main/java/com/example/HBES/service/envioService.java
+++
@@ -1,8 +0,0 @@
-package com.example.HBES.service;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class envioService {
-
-}
