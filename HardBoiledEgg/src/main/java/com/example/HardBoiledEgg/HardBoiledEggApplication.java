653969007c8f3355158a122e8f21bab723aec4a2
Momonga <lu.belmar@duocuc.cl>
2025-06-02 15:53:07 -0400

    Proyecto base de el progreso actual
    Objetivo: realizar el proyecto con nuevas dependencias y desde cero, aprendiendo de el anterior

---
+++ HardBoiledEgg/src/main/java/com/example/HardBoiledEgg/HardBoiledEggApplication.java
@@ -0,0 +1,13 @@
+package com.example.HardBoiledEgg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardBoiledEggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardBoiledEggApplication.class, args);
+	}
+
+}
---
+++ HardBoiledEgg/src/test/java/com/example/HardBoiledEgg/HardBoiledEggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HardBoiledEgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardBoiledEggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
