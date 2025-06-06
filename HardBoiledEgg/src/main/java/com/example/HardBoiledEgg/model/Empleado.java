69f26401719f4b91f88a2de50ee03783f85de50c
Momonga <lu.belmar@duocuc.cl>
2025-06-06 13:33:39 -0400

    trabajo clase 3
    06/06/25

---
+++ HardBoiledEgg/src/main/java/com/example/HardBoiledEgg/model/Empleado.java
@@ -0,0 +1,35 @@
+package com.example.HardBoiledEgg.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Empleado")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name ="TYPE", discriminatorType= DiscriminatorType.STRING, length = 20)
+public class Empleado extends Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private Integer Salario;
+
+    
+
+}
---
+++ HardBoiledEgg/src/main/java/com/example/HardBoiledEgg/model/Usuario.java
@@ -0,0 +1,39 @@
+package com.example.HardBoiledEgg.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+@MappedSuperclass
+@Table(name= "Usuario")
+@Data
+
+public abstract class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, length = 10, nullable = false)
+    private Integer run;
+
+    @Column(unique = false, length = 40, nullable = false)
+    private String nombre;
+
+    @Column(unique = false, length = 50, nullable = false)
+    private String correo;
+
+    @Column(unique = false, length = 60, nullable = false)
+    private String direccion;
+
+    @Column(unique = false, length = 11, nullable = true)
+    private String telefono;
+
+
+}
