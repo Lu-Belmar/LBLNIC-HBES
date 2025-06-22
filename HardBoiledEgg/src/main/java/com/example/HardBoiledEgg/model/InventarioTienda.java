12dbe1b46b87a602fff1bd9262dac159790a661b
Momonga <lu.belmar@duocuc.cl>
2025-06-22 19:36:44 -0400

    .:..:

--- HardBoiledEgg/src/main/java/com/example/HardBoiledEgg/model/InventarioTienda.java
+++ HardBoiledEgg/src/main/java/com/example/HardBoiledEgg/model/InventarioTienda.java
@@ -9,11 +9,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import com.example.HardBoiledEgg.model.Producto;
 
 @Entity
 @Table(name = "InventarioTienda")
@@ -32,8 +32,9 @@ public class InventarioTienda {
     @Column(nullable = false)
     private int precioLocal;
 
-    @OneToMany(mappedBy = "inventario")
-    private List<Producto> productos;
+    @ManyToOne
+    @JoinColumn(name = "producto_id")
+    private Producto producto;
 
     @ManyToOne
     @JoinColumn(name = "tienda_id")
