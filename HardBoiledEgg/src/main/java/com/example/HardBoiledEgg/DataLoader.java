c5d048471740f27730dbd8ad4f7cb4112a71815d
Momonga <lu.belmar@duocuc.cl>
2025-06-23 14:08:43 -0400

    .::...

--- HardBoiledEgg/src/main/java/com/example/HardBoiledEgg/DataLoader.java
+++ HardBoiledEgg/src/main/java/com/example/HardBoiledEgg/DataLoader.java
@@ -1,9 +1,24 @@
 package com.example.HardBoiledEgg;
 
+import com.example.HardBoiledEgg.model.Categorias;
+import com.example.HardBoiledEgg.model.Cliente;
+import com.example.HardBoiledEgg.model.Direccion;
+import com.example.HardBoiledEgg.model.Empleado;
+import com.example.HardBoiledEgg.model.Envio;
+import com.example.HardBoiledEgg.model.InventarioTienda;
+import com.example.HardBoiledEgg.model.Producto;
+import com.example.HardBoiledEgg.model.Proveedores;
+import com.example.HardBoiledEgg.model.Tienda;
+import com.example.HardBoiledEgg.model.Venta;
+import net.datafaker.Faker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+
+
+
+
 
 //@Profile("!test")
 //@Component
@@ -21,3 +36,5 @@ import org.springframework.stereotype.Component;
 
     //}
 //}
+
+
