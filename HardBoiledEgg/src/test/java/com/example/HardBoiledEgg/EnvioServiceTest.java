7af6d002408b7fbc5ef6c0f8edaba65905d31e10
Momonga <lu.belmar@duocuc.cl>
2025-06-24 22:50:20 -0400

    yestamamadaquewe

--- HardBoiledEgg/src/test/java/com/example/HardBoiledEgg/EnvioServiceTest.java
+++ HardBoiledEgg/src/test/java/com/example/HardBoiledEgg/EnvioServiceTest.java
@@ -1,4 +1,4 @@
-package test.java.com.example.HardBoiledEgg;
+package com.example.HardBoiledEgg;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,16 +6,27 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
+import com.example.HardBoiledEgg.model.Direccion;
 import com.example.HardBoiledEgg.model.Envio;
 import com.example.HardBoiledEgg.model.Venta;
+import com.example.HardBoiledEgg.repository.envioRepository;
+import com.example.HardBoiledEgg.service.direccionService;
 
 import static org.mockito.Mockito.*;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+
+
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -24,22 +35,31 @@ public class EnvioServiceTest {
     @Autowired
     private com.example.HardBoiledEgg.service.envioService envioService;
 
-    @MockBean
-    private EnvioRepository envioRepository;
+    @MockitoBean
+    private envioRepository enviorepository;
 
     @Test
     public void testFindAll(){
+
         Venta venta = new Venta();
         venta.setId(1);
         venta.setMonto(20000);
-        venta.setInventario(1);
-        venta.setCliente(1);
+        venta.setInventario(null);
+        venta.setCliente(null);
 
-        when(envioRepository.findAll()).thenReturn(List.of(new Envio(1,10,"Av. Juanin",venta)));
+        Direccion direccion = new Direccion() {
+            
+        };
 
-        List<Envio> envios = envioService.findAll();
+        List<Envio> envios = envioService.getEnvios();
+
+
+    
+        when(enviorepository.findAll()).thenReturn(List.<Envio>of(new Envio(1,Date.valueOf("21"),)));
+
+
 
         assertNotNull(envios);
-        assertEquals(1,ventas.size());
+        assertEquals(1,envios.size());
     }
 }
